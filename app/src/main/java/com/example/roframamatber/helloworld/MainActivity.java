49fc42abc196c3ef741e2502301c1243c0f572dd
rociomatabuenaberdugo <holaadios2411@gmail.com>
2018-10-03 10:58:41 +0200

    Actualizado

--- app/src/main/java/com/example/roframamatber/helloworld/MainActivity.java
+++ app/src/main/java/com/example/roframamatber/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.roframamatber.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        msg = (TextView) findViewById(R.id.textView);
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                msg.setText(R.string.texto_pulsado);
+            }
+        });
     }
 }
