de4acc4a8c9785ffdc5f5b6ed3dc1b376659dc6c
rociomatabuenaberdugo <holaadios2411@gmail.com>
2018-10-03 10:46:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/roframamatber/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.roframamatber.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView msg;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
